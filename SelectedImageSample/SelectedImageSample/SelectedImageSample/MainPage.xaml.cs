3e1a057175275633106f53f5e9869cc1298e5250
Julian Ewers <ewerspej@tcd.ie>
2022-10-17 12:27:49 +0200

    added code

---
+++ SelectedImageSample/SelectedImageSample/SelectedImageSample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SelectedImageSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectImage(object sender, EventArgs e)
+        {
+            if (sender is ImageButton imageButton)
+            {
+                VisualStateManager.GoToState(imageButton, "Selected");
+            }
+        }
+    }
+}
